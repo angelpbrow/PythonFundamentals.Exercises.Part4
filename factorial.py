0124ee5ffc021424daf289c79cd6aef807ea05a0
Angelika Brown <angelikpbrown@gmail.com>
2025-03-12 11:50:37 -0400

    factorial

---
+++ factorial.py
@@ -0,0 +1,16 @@
+#x = input("Insert a number")
+#for i in range (30):
+#          result = i * x 
+#           print (result)
+
+
+n = 20
+result = 1
+for i in range (1, n + 1):
+    result = result * i
+
+print(result)
+
+
+
+        
